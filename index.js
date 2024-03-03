abc8b76858974dce93a64236003c4701ab6253da
hidd666 <rohidayat666@gmail.com>
2024-03-03 20:15:13 +0700

    123

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static(__dirname + "/public"));
 app.get("*", function(request, response) {
   response.send("Tampil abangku");
 });
 
-app.use(express.static(__dirname + "/public"));
 
 app.listen(3000);
